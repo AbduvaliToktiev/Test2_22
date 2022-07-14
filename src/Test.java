b13b2b6f28796d0a6ab3441be0c194cd58be4942
Admin <abdutokt2004>
2022-07-14 13:42:18 +0600

    test2_22

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,18 @@
+import static java.lang.System.out;
+
+// Код для дублирования строк
 public class Test {
     public static void main(String[] args) {
-
+        String s = "adsADASDASD";
+        String[] rows = s.split("\n");
+        String[] words = s.split(" ");
+        for (String item : rows) {
+            String changed = null;
+            for(String item2:words) {
+                changed = item.replaceAll(item2, item2+" "+item2);
+            }
+            out.println(changed);
+        }
     }
 }
+
